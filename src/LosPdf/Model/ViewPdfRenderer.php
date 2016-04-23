b8a9bb8110fd109d4b000c3d04bf5bbccfda6f0d
Leandro Silva <lansoweb@hotmail.com>
2016-04-23 14:02:32 -0300

    Adding compatibility with zend-mvc 2.7

--- src/LosPdf/Model/ViewPdfRenderer.php
+++ src/LosPdf/Model/ViewPdfRenderer.php
@@ -25,12 +25,24 @@ class ViewPdfRenderer implements FactoryInterface
 {
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        $viewManager = $serviceLocator->get('ViewManager');
+        /* @var $viewResolver \Zend\View\Resolver\AggregateResolver */
+        $viewResolver = $serviceLocator->get('ViewResolver');
+        
+        /* @var $viewRenderer \Zend\View\Renderer\PhpRenderer */
+        $viewRenderer = $serviceLocator->get('ViewRenderer');
+
+        if ($viewRenderer == null) {
+            $viewManager = $serviceLocator->get('ViewManager');
+            if (!method_exists($viewManager, 'getRenderer')) {
+                throw new \RuntimeException('Unable to find a Renderer');
+            }
+            $viewRenderer = $viewManager->getRenderer();
+        }
 
         //Later, this will be an option in config (mpdf, dompdf, etc)
         $pdfRenderer = new MpdfRenderer();
-        $pdfRenderer->setResolver($viewManager->getResolver());
-        $pdfRenderer->setRenderer($viewManager->getRenderer());
+        $pdfRenderer->setResolver($viewResolver);
+        $pdfRenderer->setRenderer($viewRenderer);
 
         return $pdfRenderer;
     }
